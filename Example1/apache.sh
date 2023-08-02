e76df18976ca322f03e1744a2e6e83844af44bdf
shis2hir6410@yahoo.com <shis2hir6410@yahoo.com>
2023-08-02 09:28:37 +0000

    apachemodified pushed

--- Example1/apache.sh
+++ Example1/apache.sh
@@ -3,3 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start
+echo "<h1> This page server has been deployed by teraformcloud </h1>" | sudo tee /var/www/html/index.html
