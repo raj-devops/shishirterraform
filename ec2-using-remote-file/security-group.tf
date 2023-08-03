ea7587349c6fc82bc2087b83bf45a467f34158b8
shis2hir6410@yahoo.com <shis2hir6410@yahoo.com>
2023-08-03 10:22:29 +0000

    mainsg file11 has pushed

--- ec2-using-remote-file/security-group.tf
+++ ec2-using-remote-file/security-group.tf
@@ -9,8 +9,8 @@ resource "aws_security_group" "terraform-cloud-shishir-group" {
     description = "allow ssh for port 22"
     from_port   = 22
     to_port     = 22
-    protocol    = tcp
-    cidr_blocks  = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
 
@@ -19,8 +19,8 @@ resource "aws_security_group" "terraform-cloud-shishir-group" {
     description = "allow http port 80"
     from_port   = 80
     to_port     = 80
-    protocol    = tcp
-    cidr_blocks  = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
 
@@ -30,7 +30,7 @@ resource "aws_security_group" "terraform-cloud-shishir-group" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks  = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
 
